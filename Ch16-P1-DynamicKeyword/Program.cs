89e60bff6563543c8999be5cde03d4ee05288499
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-11-19 19:23:10 +0500

    git ingore added

--- Ch16-P1-DynamicKeyword/Program.cs
+++ Ch16-P1-DynamicKeyword/Program.cs
@@ -10,7 +10,15 @@ namespace Ch16_P1_DynamicKeyword
     {
         static void Main(string[] args)
         {
+            dynamic d = "hello";
+            Console.WriteLine(d.GetType());
+           // d++;
 
+            d = 12;
+
+            d++;
+            Console.WriteLine(d.GetType());
+            Console.ReadLine();
         }
     }
 }
