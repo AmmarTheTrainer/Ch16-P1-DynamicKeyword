298110a68fbd95a61e211386c5f9a0b84ac57147
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-11-20 13:19:17 +0500

    code update 1 from head office

--- Ch16-P1-DynamicKeyword/Program.cs
+++ Ch16-P1-DynamicKeyword/Program.cs
@@ -10,15 +10,68 @@ namespace Ch16_P1_DynamicKeyword
     {
         static void Main(string[] args)
         {
-            dynamic d = "hello";
-            Console.WriteLine(d.GetType());
-           // d++;
+            // dynamic d = "hello";
+            // Console.WriteLine(d.GetType());
+            //// d++;
+            // d = 12;
+            // d++;
+            // Console.WriteLine(d.GetType());
 
-            d = 12;
+            // ImplicitlyTypedVariable();
+            //UseObjectVarible();
+            //PrintThreeStrings();
+            //ChangeDynamicDataType();
+            InvokeMembersOnDynamicData();
 
-            d++;
-            Console.WriteLine(d.GetType());
             Console.ReadLine();
+        }
+
+        private static void InvokeMembersOnDynamicData()
+        {
+            dynamic textData1 = "Hello";
+            Console.WriteLine(textData1.ToUpper());
+            // You would expect compiler errors here!
+            // But they compile just fine.
+            Console.WriteLine(textData1.toupper());
+            Console.WriteLine(textData1.Foo(10, "ee", DateTime.Now));
+        }
+
+        private static void ImplicitlyTypedVariable()
+        {
+            // a is of type List<int>.
+            var a = new List<int> { 90 };
+            // This would be a compile-time error!
+           // a = "Hello";
+        }
+
+        static void UseObjectVarible()
+        {
+            // Assume we have a class named Person.
+            object o = new Person() { FirstName = "Mike", LastName = "Larson" };
+            // Must cast object as Person to gain access
+            // to the Person properties.
+            Console.WriteLine("Person's first name is {0}", ((Person)o).FirstName);
+        }
+
+        static void PrintThreeStrings()
+        {
+            var s1 = "Greetings";
+            object s2 = "From";
+            dynamic s3 = "Minneapolis";
+            Console.WriteLine("s1 is of type: {0}", s1.GetType());
+            Console.WriteLine("s2 is of type: {0}", s2.GetType());
+            Console.WriteLine("s3 is of type: {0}", s3.GetType());
+        }
+        static void ChangeDynamicDataType()
+        {
+            // Declare a single dynamic data point
+            // named "t".
+            dynamic t = "Hello!";
+            Console.WriteLine("t is of type: {0}", t.GetType());
+            t = false;
+            Console.WriteLine("t is of type: {0}", t.GetType());
+            t = new List<int>();
+            Console.WriteLine("t is of type: {0}", t.GetType());
         }
     }
 }
